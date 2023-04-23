3db4c0a25ca205c2ed6fd363d4803f2e7e40c204
Kushal293 <kushalkm293@gmail.com>
2023-04-23 23:03:53 +0530

    getFollowers and updateInterests

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -36,4 +36,50 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
             message: "Profile has been updated",
             user: newuser,
         });
+});
+
+export const updateUserInterest = asyncHandler(async (req, res) => {
+    const { user } = req.user;
+    
+    if (!user) {
+        throw new CustomError("user not found", 404);
+    }
+
+    const newuser = await User.findOneAndUpdate({
+        email: user.email,
+    }, {
+        interests: req.body.interests,
+    });
+
+    if (!newuser) {
+        throw new CustomError("User not found", 401);
+    }
+
+    res.status(200).json({
+        success: true,
+        message: "Interests has been added successfully",
+        newuser,
+    });
+
+});
+
+export const getFollowers = asyncHandler(async (req, res) => {
+    const { user } = req.user;
+    
+    if (!user) {
+        throw new CustomError("user not found", 404);
+    }
+
+    const newUser = await User.findOne({
+        email: user.email
+    });
+
+    if (!newUser) {
+        throw new CustomError("user not found", 404);
+    }
+
+    res.status(200).json({
+            success: true,
+            followers: newuser.followers,
+        })
 })
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from 'express';
 const userRouter = express.Router(); 
 import { getUser, updateUserProfile, updateUserInterest, getFollowers } from '../controllers/userController.js';
-import { resetPassword } from '../controllers/authController.js';
+import { changePassword, resetPassword } from '../controllers/authController.js';
 import { isLogged } from '../middlewares/authMiddleware.js';
 
 userRouter.get("/profile", isLogged, getUser);
-userRouter.get("/profile/followers", isLogged, getFollowers);
 userRouter.put("/profile/profile", isLogged , updateUserProfile);
-userRouter.put("/profile/change", isLogged ,changePassword);
+userRouter.put("/profile/change", isLogged , changePassword);
+userRouter.get("/profile/followers", isLogged, getFollowers);
 userRouter.put("/profile/interests", isLogged ,updateUserInterest);
 
 export default userRouter;
